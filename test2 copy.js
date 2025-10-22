d7d1103fc7864e4448ec569970a793a53ed68834
Wendy Calizaya <wendycalizayaperez@gmail.com>
2025-10-22 15:41:12 -0400

    new function

--- test2 copy.js
+++ test2 copy.js
@@ -1,3 +1,12 @@
-function calc(a,b){
-return a+b
-}
+var Circulo = function(nuevoRadio){
+  this.radio = nuevoRadio;
+  this.area =  function(){
+    return Math.pow(this.radio, 2) * Math.PI;
+  }
+};
+
+a = new Circulo(5.0);
+b = new Circulo(2);
+
+console.log("Área de a: "+ a.area());
+console.log("Área de b: "+ b.area());
